8bc2deebd05b31a2d4b28f2a657740430b74170d
maikon <maikonschafrnski12@gmail.com>
2023-11-21 21:22:50 -0300

    commit: portfolio com scroll to top

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+import ScrollToTop from "react-scroll-to-top";
+
+const Footer = () => {
+  return (
+    <div>
+      <ScrollToTop className="scrollTop" smooth top="50" />
+
+      <footer className="footer">
+        Copyright © 2023 Maikon Schafranski. Todos os direitos reservados.
+      </footer>
+    </div>
+  );
+};
+export default Footer;
